5fe3d64561f03ba3952dfa22f10e0f4d3eedf76e
Shaheer Zia Qazi <shaheerzia22@gmail.com>
2025-07-11 22:00:14 +0500

    ddd

--- lib/constants/app_fonts.dart
+++
@@ -1,6 +0,0 @@
-import 'package:google_fonts/google_fonts.dart';
-
-class AppFonts {
-  static final URBANIST = 'Urbanist';
-  // static final URBANIST = GoogleFonts.poppins().fontFamily;
-}
--- lib/utils/global_instances.dart
+++
@@ -1,5 +0,0 @@
-import 'package:candid/controller/profile_controller.dart';
-import 'package:candid/controller/quiz_controller/quiz_controller.dart';
-
-ProfileController profileController = ProfileController.instance;
-QuizController quizController = QuizController.instance;
